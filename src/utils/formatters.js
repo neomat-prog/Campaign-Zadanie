4db3969fbe503b640949d5502cb19ba01750e4b5
neomat-prog <thegreyliongaming@gmail.com>
2025-06-11 18:04:25 +0200

    Add utility function for formatting currency to PLN

---
+++ src/utils/formatters.js
@@ -0,0 +1,8 @@
+export const formatCurrency = (amount) => {
+  return new Intl.NumberFormat("pl-PL", {
+    style: "currency",
+    currency: "PLN",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(amount);
+};
